7e4dacba18a8f9acb1a3919e6d51a117d1540132
hupptechnologies <git@hupp.in>
2025-02-25 13:37:51 +0530

    Add README.md file

--- src/interface/common.ts
+++
@@ -1,9 +0,0 @@
-import { Optional } from 'sequelize';
-
-export interface QuertyAttributes {
-	search: string,
-	limit: number,
-	offset: number
-}
-
-export type TQuery = Optional<QuertyAttributes, 'search' | 'limit' | 'offset' >;
